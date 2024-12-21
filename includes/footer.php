d782c0720c84268105b5b82845d8fc53e96df2e6
Gürkan <ganbarli@hotmail.com>
2024-12-21 12:14:04 +0300

    Footer changed.
    Bootstrap footer added.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,13 @@
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Gürkan\'ın Blogu</p>
-    </footer>
+		</div>
+	</div>
+</div>
+
+<footer class="footer mt-auto py-3 bg-body-secondary">
+    <p class="text-center text-body-secondary border-bottom pb-3 mb-3"><a href="https://github.com/anbarli/PHP-MBBS" class="btn btn-dark btn-sm" target="_blank">PHP-MBBS</a> ile oluşturuldu</p>
+    <p class="text-center text-body-secondary">&copy; <?php echo date("Y"); ?> <?php echo SITE_NAME; ?></p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+
 </body>
 </html>
